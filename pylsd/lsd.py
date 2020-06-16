89e2e846f93f064005b312b863fd7657689ef1ae
YUSEOK JEONG <ghaao78@gmail.com>
2020-06-16 15:17:31 +0900

    Update lsd.py

--- pylsd/lsd.py
+++ pylsd/lsd.py
@@ -5,7 +5,7 @@
 # @Link    : https://github.com/primetang/pylsd
 # @Version : 0.0.1
 
-from .bindings.lsd_ctypes import *
+from pylsd.bindings.lsd_ctypes import *
 
 
 def lsd(src):
@@ -18,8 +18,8 @@ def lsd(src):
 
     lens = len(src)
     src = (ctypes.c_double * lens)(*src)
-    lsdlib.lsdGet(src, ctypes.c_int(rows), ctypes.c_int(cols), temp)
-
+    # lsdlib.lsdGet(src, ctypes.c_int(rows), ctypes.c_int(cols), temp)
+    lsdlib.lsdGet(src, ctypes.c_int(rows), ctypes.c_int(cols), ctypes.c_char_p(temp))
     fp = open(temp, 'r')
     cnt = fp.read().strip().split(' ')
     fp.close()
@@ -29,5 +29,5 @@ def lsd(src):
     dim = int(cnt[1])
     lines = np.array([float(each) for each in cnt[2:]])
     lines = lines.reshape(count, dim)
-
+    
     return lines
